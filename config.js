770de21265cb5723a5deaa74bda3a3c2b0b6c823
罗凯#A02637 <346195459@qq.com>
2023-01-12 14:27:12 +0800

    update

--- config.js
+++ config.js
@@ -2,9 +2,10 @@ module.exports = {
   host: "http://gerrit.casstime.net:8087",
   user: "A02637",
   pass: "Luokai_123",
+  headless: true,
 
-  target: "罗凯",
+  target: "吴占展",
 
   /** url加密 */
-  encode: true,
+  encode: false,
 };
